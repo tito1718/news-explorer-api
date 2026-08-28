166ab8dcb160f406a57aea45cca2a115b8b3db73
Cesar Chirino <tito1718@aol.com>
2026-08-27 22:39:03 -0400

    feat: add API security logging and testing

--- app.js
+++ app.js
@@ -2,17 +2,34 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { rateLimit } = require('express-rate-limit');
+
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { NotFoundError } = require('./errors');
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    message: 'Too many requests. Please try again later.',
+  },
+});
 
 const {
   PORT = 3000,
   DATABASE_URL = 'mongodb://127.0.0.1:27017/news-explorer',
 } = process.env;
 
+app.use(helmet());
+app.use(requestLogger);
+app.use(limiter);
 app.use(express.json());
 
 app.use('/', routes);
@@ -21,6 +38,7 @@ app.use((_req, _res, next) => {
   next(new NotFoundError());
 });
 
+app.use(errorLogger);
 app.use(errorHandler);
 
 mongoose
